e6920022a1256712f6bd9fce501ebd430d43f98e
artur <rurkss@gmail.com>
2018-08-20 16:31:27 +0300

    driver documentation

--- lib/xrates/driver.rb
+++ lib/xrates/driver.rb
@@ -1,10 +1,26 @@
 module Xrates
+  #
+  # Driver for importing different
+  # currency adapters to storage (file either redis, etc.)
+  # May be set as cron job once per day
+  #
   class Driver
 
     def initialize()
       @adapters = []
     end
 
+    #
+    # Collect adapters to run import
+    # @param [Object] adapter. The object of Xrate::Adapter
+    #
+    # @return [Array]
+    #
+    # @example
+    #  driver = Xrates::Driver.new
+    #  driver << Xrates::Adapter::Cbr.new
+    #  driver << Xrates::Adapter::Fixer.new
+    #
     def <<(adapter)
       @adapters << adapter
     end
